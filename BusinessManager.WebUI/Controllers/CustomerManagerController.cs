7f67471bf94589e3cf205c9439fcfdb59abaadc9
uhnews <54126262+uhnews@users.noreply.github.com>
2019-09-10 21:29:42 -0700

    Small Changes made to POSTransaction/Checkout UI; Customers Security added allowing access for Admin and POSAttendant roles

--- BusinessManager.WebUI/Controllers/CustomerManagerController.cs
+++ BusinessManager.WebUI/Controllers/CustomerManagerController.cs
@@ -1,12 +1,12 @@
 ﻿using BusinessManager.Core.Contracts;
 using BusinessManager.Core.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 
 namespace BusinessManager.WebUI.Controllers
 {
+    [Authorize(Roles = "Admin, POSAttendant")]
     public class CustomerManagerController : Controller
     {
         IRepository<Customer> context;
